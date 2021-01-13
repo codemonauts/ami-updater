acfc3aa1915bb88907f7437ead40e008cdf30a41
Felix Breidenstein <felix@codemonauts.com>
2021-01-13 11:23:18 +0100

    Fix formatting

--- main.py
+++ main.py
@@ -3,40 +3,40 @@ import boto3
 
 
 def find_latest_ami(search_string):
-    ec2_client = boto3.client('ec2')
+    ec2_client = boto3.client("ec2")
     response = ec2_client.describe_images(
         Filters=[
             {
-                'Name': 'name',
-                'Values': [
+                "Name": "name",
+                "Values": [
                     search_string,
-                ]
+                ],
             },
-        ])
+        ]
+    )
     try:
-        ami_list = response['Images']
-        latest_ami = sorted(ami_list, key=lambda k: k['CreationDate'], reverse=True)[0]
+        ami_list = response["Images"]
+        latest_ami = sorted(ami_list, key=lambda k: k["CreationDate"], reverse=True)[0]
     except:
         return None
 
-    return latest_ami['ImageId']
+    return latest_ami["ImageId"]
 
 
 def lambda_handler(event, context):
-    ec2 = boto3.client('ec2')
-    response = ec2.describe_launch_templates(
-        Filters=[{"Name": "tag-key", "Values": ["ami-search-string"]}])
+    ec2 = boto3.client("ec2")
+    response = ec2.describe_launch_templates(Filters=[{"Name": "tag-key", "Values": ["ami-search-string"]}])
     templates = response["LaunchTemplates"]
     if len(templates):
         print("Found {} launch templates".format(len(templates)))
     else:
         print("Found no launch templates with the 'ami-search-string' tag.")
-        return 'done'
+        return "done"
 
     for template in templates:
         template_id = template["LaunchTemplateId"]
         name = template["LaunchTemplateName"]
-        print("{}: ".format(name), end='')
+        print("{}: ".format(name), end="")
         ami_id = None
         for t in template["Tags"]:
             if t["Key"] == "ami-search-string":
@@ -45,11 +45,10 @@ def lambda_handler(event, context):
 
         if not ami_id:
             print("Can't find an AMI with this search string. Exiting")
-            return 'error'
+            return "error"
 
         # Get $Latest version of the template
-        latest = ec2.describe_launch_template_versions(
-            LaunchTemplateId=template_id, Versions=["$Latest"])
+        latest = ec2.describe_launch_template_versions(LaunchTemplateId=template_id, Versions=["$Latest"])
         data = latest["LaunchTemplateVersions"][0]["LaunchTemplateData"]
 
         # Replace ImageId with the new one
@@ -61,11 +60,7 @@ def lambda_handler(event, context):
 
         # Create new version
         response = ec2.create_launch_template_version(
-            LaunchTemplateId=template_id,
-            SourceVersion="$Latest",
-            LaunchTemplateData={
-                "ImageId": ami_id
-            }
+            LaunchTemplateId=template_id, SourceVersion="$Latest", LaunchTemplateData={"ImageId": ami_id}
         )
 
         if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
@@ -73,5 +68,4 @@ def lambda_handler(event, context):
             print("The error was: {}".format(response))
             return "error"
 
-    return 'done'
-
+    return "done"
