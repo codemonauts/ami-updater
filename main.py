2ea3c89ddf0e868e3b824e9bff687c92ed05a950
Thomas Schulze <tom@codemonauts.com>
2024-04-27 06:06:25 +0200

    improve code and add function to delete snapshots

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 #! /usr/bin/env python3
+
+"""
+Module to update all AWS EC2 Launch Templates to the newest AMI version.
+"""
+
 import boto3
-from pprint import pprint
 
 # Number of launchtemplate versions to keep
 LIMIT = 3
 
 
 def find_latest_ami(search_string):
+    """
+    Returns the AMI ID of the latest AMI with the given search string
+    """
     ec2_client = boto3.client("ec2")
     response = ec2_client.describe_images(
         Filters=[
@@ -21,20 +28,21 @@ def find_latest_ami(search_string):
     try:
         ami_list = response["Images"]
         latest_ami = sorted(ami_list, key=lambda k: k["CreationDate"], reverse=True)[0]
-    except:
+    except Exception:
         return None
 
     return latest_ami["ImageId"]
 
 
 def lambda_handler(event, context):
+    """
+    Entrypoint for AWS Lambda
+    """
     ec2 = boto3.client("ec2")
-    response = ec2.describe_launch_templates(
-        Filters=[{"Name": "tag-key", "Values": ["ami-search-string"]}]
-    )
+    response = ec2.describe_launch_templates(Filters=[{"Name": "tag-key", "Values": ["ami-search-string"]}])
     templates = response["LaunchTemplates"]
     if len(templates):
-        print("Found {} launch templates".format(len(templates)))
+        print(f"Found {len(templates)} launch templates")
     else:
         print("Found no launch templates with the 'ami-search-string' tag.")
         return "done"
@@ -42,11 +50,11 @@ def lambda_handler(event, context):
     for template in templates:
         template_id = template["LaunchTemplateId"]
         name = template["LaunchTemplateName"]
-        print("{}: ".format(name), end="")
+        print(f"{name}: ", end="")
         ami_id = None
-        for t in template["Tags"]:
-            if t["Key"] == "ami-search-string":
-                ami_id = find_latest_ami(t["Value"])
+        for tag in template["Tags"]:
+            if tag["Key"] == "ami-search-string":
+                ami_id = find_latest_ami(tag["Value"])
                 break  # leave loop if we found the correct key
 
         if not ami_id:
@@ -54,9 +62,7 @@ def lambda_handler(event, context):
             return "error"
 
         # Get $Latest version of the template
-        latest = ec2.describe_launch_template_versions(
-            LaunchTemplateId=template_id, Versions=["$Latest"]
-        )
+        latest = ec2.describe_launch_template_versions(LaunchTemplateId=template_id, Versions=["$Latest"])
         latest_version = latest["LaunchTemplateVersions"][0]["VersionNumber"]
         data = latest["LaunchTemplateVersions"][0]["LaunchTemplateData"]
 
@@ -79,7 +85,7 @@ def lambda_handler(event, context):
 
             if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
                 print("Aborting due to an error while creating the new launch tempalte")
-                print("The error was: {}".format(response))
+                print(f"The error was: {response}")
                 return "error"
 
         # Update default version to latest version
@@ -91,29 +97,44 @@ def lambda_handler(event, context):
 
         # we can't purge old versions if the counter is less then LIMIT
         if latest_version < LIMIT:
-            continue
+            return "done"
 
         # Cleanup of old template versions
         versions = ec2.describe_launch_template_versions(
             LaunchTemplateId=template_id, MaxVersion=str(latest_version - LIMIT)
         ).get("LaunchTemplateVersions", [])
 
-        for v in versions:
-            ami_id = v["LaunchTemplateData"]["ImageId"]
-            num = v["VersionNumber"]
-            print("Deleting version {} with attached AMI {}".format(num, ami_id))
+        for version in versions:
+            ami_id = version["LaunchTemplateData"]["ImageId"]
+            num = version["VersionNumber"]
+            ami_details = ec2.describe_images(ImageIds=[ami_id])
+            print(f"Deleting version {num} with attached AMI {ami_id} and snapshots")
+            # First: Deregister AMI
             try:
                 ec2.deregister_image(ImageId=ami_id)
+                print(f"Deregistered AMI {ami_id}")
             except Exception as ex:
-                print("Couldn't delete AMI {}".format(ami_id))
+                print(f"Couldn't deregister AMI {ami_id}")
                 print(f"Reason: {ex}")
+            # Second: Delete corresponding version from launch template
             try:
                 ec2.delete_launch_template_versions(
                     LaunchTemplateId=template_id,
                     Versions=[str(num)],
                 )
+                print(f"Removed version {num} from launch template {template_id}")
             except Exception as ex:
-                print("Couldn't delete Launchtemplate {}".format(template_id))
+                print(f"Couldn't remove version {num} from launch template {template_id}")
+                print(f"Reason: {ex}")
+            # Third: Delete all snapshots of the AMI
+            try:
+                block_devices = ami_details["Images"][0]["BlockDeviceMappings"]
+                for block_device in block_devices:
+                    snapshot_id = block_device["Ebs"]["SnapshotId"]
+                    ec2.delete_snapshot(SnapshotId=snapshot_id, DryRun=False)
+                    print(f"Deleted snapshot {snapshot_id} of AMI {ami_id}")
+            except Exception as ex:
+                print(f"Couldn't delete snapshot {snapshot_id} of AMI {ami_id}")
                 print(f"Reason: {ex}")
 
     return "done"
